836f813a820ba317bb83ce66eb8550596d718db2
Lemione Yang <101872956+lemiones@users.noreply.github.com>
2026-07-11 01:16:04 +0800

    Update sync-singbox-profile.js

--- Surge/sync-singbox-profile.js
+++ Surge/sync-singbox-profile.js
@@ -1,1 +1,40 @@
+const { exec } = require("child_process");
 
+function run(cmd) {
+    return new Promise((resolve)=>{
+        exec(cmd,(err,stdout,stderr)=>{
+            resolve({
+                err,
+                stdout,
+                stderr
+            });
+        });
+    });
+}
+
+
+(async()=>{
+
+    // console.log("Surge config updated");
+
+    let result = await run(
+        "/Users/lemione/.config/singbox/singbox_connfig_update.sh"
+    );
+
+    if(result.err){
+        $notification.post(
+            "Sing-Box配置同步失败",
+            "",
+            result.stderr
+        );
+    }else{
+        $notification.post(
+            "Sing-Box配置同步完成",
+            "",
+            result.stdout
+        );
+    }
+
+    $done();
+
+})();
